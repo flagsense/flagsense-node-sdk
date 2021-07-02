14c35bab47f40df21a453f8fa9ace6014a41a910
Flagsense <senseflag@gmail.com>
2021-07-02 18:08:50 +0530

    v1.0.2

--- index.js
+++ index.js
@@ -2,28 +2,22 @@ const FlagsenseService = require('./services/flagsense');
 const FSUser = require('./model/FSUser');
 const FSFlag = require('./model/FSFlag');
 
-class Flagsense {
-	constructor() {
-		const flagsenseServiceMap = {};
+const flagsenseServiceMap = {};
 
-		this.createService = function (sdkId, sdkSecret, environment) {
-			if (!flagsenseServiceMap.hasOwnProperty(sdkId))
-				flagsenseServiceMap[sdkId] = new FlagsenseService(sdkId, sdkSecret, environment);
-			return flagsenseServiceMap[sdkId];
-		}
-
-		this.user = function (userId, attributes) {
-			return new FSUser(userId, attributes);
-		}
-
-		this.flag = function (flagId, defaultKey, defaultValue) {
-			return new FSFlag(flagId, defaultKey, defaultValue);
-		}
-	}
-
-	// Below methods can be used on instance returned from createService method
-	// initializationComplete()
-	// getVariation(fsFlag, fsUser)
+exports.createService = function (sdkId, sdkSecret, environment) {
+	if (!flagsenseServiceMap.hasOwnProperty(sdkId))
+		flagsenseServiceMap[sdkId] = new FlagsenseService(sdkId, sdkSecret, environment);
+	return flagsenseServiceMap[sdkId];
 }
 
-module.exports = new Flagsense();
+exports.user = function (userId, attributes) {
+	return new FSUser(userId, attributes);
+}
+
+exports.flag = function (flagId, defaultKey, defaultValue) {
+	return new FSFlag(flagId, defaultKey, defaultValue);
+}
+
+// Below methods can be used on instance returned from createService method
+// initializationComplete()
+// getVariation(fsFlag, fsUser)
